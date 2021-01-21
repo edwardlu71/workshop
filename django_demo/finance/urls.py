c6c1ae5cebac0f9df9c4274cb44dd49181e90961
Edward Lu <luqiyi@gmail.com>
2021-01-21 04:50:52 +0000

    remove rubbish

--- django_demo/finance/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='finance_index'),
-    path('timeseries', views.timeseries, name='finance_timeseries'),
-    path('form', views.form, name='finance_form'),
-]
